26237b43c6ac6b21b0e272216005708a9ca536db
Ivan Borovyk <Ivan_Borovyk@epam.com>
2023-10-14 12:58:48 +0300

    task 9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btnChangeColor = document.querySelector('.change-color');
+const colorSpan = document.querySelector('.color');
+
+const onBtnChangeColorClick = () => {
+  const color = getRandomHexColor();
+  document.body.style.backgroundColor = color;
+  colorSpan.textContent = color;
+}
+
+btnChangeColor.addEventListener('click', onBtnChangeColorClick);
