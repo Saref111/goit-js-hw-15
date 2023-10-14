cde87c0290055b045b3f35581a61a80a2a7ef8dd
Ivan Borovyk <Ivan_Borovyk@epam.com>
2023-10-14 13:05:48 +0300

    10 task

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -3,3 +3,33 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const controls = document.querySelector('#controls');
+const createButton = controls.querySelector('[data-create]');
+const destroyButton = controls.querySelector('[data-destroy]');
+const input = controls.querySelector('input');
+const boxes = document.querySelector('#boxes');
+
+const createBoxes = () => {
+  const amount = input.value;
+  const divs  = [];
+
+  for (let i = 0; i < amount; i += 1) {
+    const div = document.createElement('div');
+    const size = 30 + (i * 10);
+    div.style.width = `${size}px`;
+    div.style.height = `${size}px`;
+    div.style.backgroundColor = getRandomHexColor();
+    divs.push(div);
+  }
+
+  boxes.append(...divs);
+};
+
+const destroyBoxes = () => {
+  boxes.innerHTML = '';
+};
+
+
+createButton.addEventListener('click', createBoxes);
+destroyButton.addEventListener('click', destroyBoxes);
