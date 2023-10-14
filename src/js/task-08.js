e4cb6dcd72a0b8ea8c738dd0eef2f35c289245b4
Ivan Borovyk <Ivan_Borovyk@epam.com>
2023-10-14 12:55:26 +0300

    task 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,19 @@
+const formElement = document.querySelector('.login-form');
 
+const onFormSubmit = (event) => {
+    event.preventDefault();
+    const inputs = Array.from(formElement.querySelectorAll('input'));
+
+    if (inputs.some((input) => input.value === '')) {
+        alert('Всі поля повинні бути заповнені.');
+        return;
+    };
+
+    const formData = new FormData(event.currentTarget);
+    formElement.reset();
+    formData.forEach((value, name) => {
+        console.log(`${name}: ${value}`);
+    });
+};
+
+formElement.addEventListener('submit', onFormSubmit);
