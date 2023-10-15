d3b7748aa47f0431cbee68b896b3f9524b0aec98
Ivan Borovyk <Ivan_Borovyk@epam.com>
2023-10-15 11:27:53 +0300

    fix 7 task

--- js/task-07.js
+++ js/task-07.js
@@ -1,5 +1,6 @@
 const inputRangeElement = document.querySelector('#font-size-control');
 const textElement = document.querySelector('#text');
+textElement.style.fontSize = `${inputRangeElement.value}px`;
 
 const onInputRangeChange = (event) => {
     textElement.style.fontSize = `${event.currentTarget.value}px`;
